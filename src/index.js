0287ebd44b15144bc47477b6539ec8e298250bd8
Jorge Garcia <jorge.garcia@openbravo.com>
2021-12-13 09:22:43 +0100

    Add messages to indicate a user has been connected or left the chat

--- src/index.js
+++ src/index.js
@@ -18,8 +18,14 @@ io.on('connection', (socket) => {
 
   socket.emit('message', 'Welcome!');
 
+  socket.broadcast.emit('message', 'A new user has joined!');
+
   socket.on('sendMessage', (message) => {
     io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
   });
 });
 
