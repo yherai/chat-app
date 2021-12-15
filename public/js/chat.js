2a1612d3b68d6ae9a8e85ffdcf7cf413f681d1b0
Jorge Garcia <jorge.garcia@openbravo.com>
2021-12-15 10:12:29 +0100

    Add conditional autoscrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,7 +18,29 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
-console.log(username, room);
+
+const autoscrool = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
+
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // Scroll top position
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
 
 socket.on('message', (message) => {
   console.log(message);
@@ -30,6 +52,7 @@ socket.on('message', (message) => {
   });
 
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscrool();
 });
 
 socket.on('locationMessage', (message) => {
@@ -42,6 +65,7 @@ socket.on('locationMessage', (message) => {
   });
 
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscrool();
 });
 
 socket.on('roomData', ({ room, users }) => {
